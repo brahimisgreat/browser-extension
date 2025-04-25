92714a7cfd71f82cc83c94af05993a17468faa0d
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2025-04-25 02:03:00 -0400

    fdfd

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const btn1 = document.getElementById('all').innerHTML;
-const btn2 = document.getElementById('active').innerHTML;
-const btn3 = document.getElementById('inactive').innerHTML;
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector('btn2');
+const btn3 = document.querySelector('btn3');
+
+btn1.addEventListener("click", btn1Click);
+btn2.addEventListener("click", btn2Click);
+
+function btn1Click() {
+    btn1.classList.toggle("active");
+    btn2.classList.remove("active");    
+    btn3.classList.remove("active");
+}
+
+function btn2Click(){}
